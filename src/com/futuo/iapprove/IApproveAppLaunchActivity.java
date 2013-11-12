a696ee8664898cea055ed352b75037b95bf6fb67
Ares <chelsea.zhai@gmail.com>
2013-11-12 14:32:10 +0800

    complete init approve android project

---
+++ src/com/futuo/iapprove/IApproveAppLaunchActivity.java
@@ -0,0 +1,28 @@
+package com.futuo.iapprove;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+
+import com.richitec.commontoolkit.activityextension.AppLaunchActivity;
+
+public class IApproveAppLaunchActivity extends AppLaunchActivity {
+
+	@Override
+	public Drawable splashImg() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Intent intentActivity() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean didFinishLaunching() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
