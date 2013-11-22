f0559729ee1cab160ed6dc3eb4d5226992040ad6
Ares <chelsea.zhai@gmail.com>
2013-11-22 18:38:24 +0800

    11.22 tmp saved

--- src/com/futuo/iapprove/IApproveAppLaunchActivity.java
+++ src/com/futuo/iapprove/IApproveAppLaunchActivity.java
@@ -3,6 +3,7 @@ package com.futuo.iapprove;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 
+import com.futuo.iapprove.settings.AccountSetting4FirstActivity;
 import com.futuo.iapprove.tab7tabcontent.IApproveTabActivity;
 import com.richitec.commontoolkit.activityextension.AppLaunchActivity;
 
@@ -20,7 +21,9 @@ public class IApproveAppLaunchActivity extends AppLaunchActivity {
 		Intent _targetIntentActivity = new Intent(this,
 				IApproveTabActivity.class);
 
-		//
+		// test by ares
+		// go to account setting activity
+		_targetIntentActivity = new Intent(this, AccountSetting4FirstActivity.class);
 
 		// go to target activity
 		return _targetIntentActivity;
